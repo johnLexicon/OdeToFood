c9d637b8cb3e7c878077b379b84234c7ffebcaf4
John Lundgren <john.paul.lundgren@gmail.com>
2018-12-19 16:52:09 +0100

    Added AboutController.

---
+++ OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+    public class AboutController
+    {
+        public string Phone()
+        {
+            return "070-0490691545";
+        }
+
+        public string Address()
+        {
+            return "Svedjegatan 33 lgh 1101 Borås";
+        }
+    }
+}
